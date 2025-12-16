9304c792b57d6a68eb8ef0a8e8fff5b2f613952c
Deák Ruben <55796855+RubiMaistro@users.noreply.github.com>
2025-12-16 19:14:08 +0100

    feat(Application): add order repository

---
+++ src/Restaurant.Application/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Restaurant.Domain.Entities;
+
+namespace Restaurant.Application.Interfaces.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task AddAsync(Order order, CancellationToken cancellationToken);
+        Task<Order?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
