bdc429895fb69cad14fea4a9d2e63cfe9f8e5ffe
Deák Ruben <55796855+RubiMaistro@users.noreply.github.com>
2025-12-31 21:39:13 +0100

    feat(application): add CreateOrderValidator and OrderService scoped services

--- src/Restaurant.Application/Extensions/ServiceCollectionExtensions.cs
+++ src/Restaurant.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurant.Application.DTOs;
+using Restaurant.Application.Interfaces.Services;
+using Restaurant.Application.Services;
+using Restaurant.Application.Validators;
 
 namespace Restaurant.Application.Extensions
 {
@@ -6,6 +11,9 @@ namespace Restaurant.Application.Extensions
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddScoped<IValidator<CreateOrderDto>, CreateOrderValidator>();
+            services.AddScoped<IOrderService, OrderService>();
+
             return services;
         }
     }
